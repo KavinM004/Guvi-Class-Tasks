c68cb42cdf8116e56d7bc008c89a38a91732f4ab
karthick <karthickkavin004@gmail.com>
2023-06-28 15:28:37 +0530

    Day-3

---
+++ Day-3/Compare2objects.js
@@ -0,0 +1,10 @@
+let obj1 = {Name : "Person1",age : 5};
+let obj2 = {age : 5,Name : "Person1"};
+
+let kavin = (obj1,obj2) => {
+    let obj1 = Object.keys(obj1);
+    let obj2 = Object.keys(obj2);
+    if()
+}
+
+
