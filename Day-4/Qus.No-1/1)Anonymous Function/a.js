9711665641b121d11fb1b5212034a9b462aeaee6
karthick <karthickkavin004@gmail.com>
2023-06-29 15:33:14 +0530

    Day-4

---
+++ Day-4/Qus.No-1/1)Anonymous Function/a.js
@@ -0,0 +1,9 @@
+let kavin = function(){
+    let karthick=[1,2,3,4,5,6];
+    for(let i=0;i<karthick.length;i++){
+        if(karthick[i]%2!==0){
+            console.log(karthick[i]);
+        }
+    }
+}
+kavin();
---
+++ Day-4/Qus.No-1/1)IIFE/a.js
@@ -0,0 +1,8 @@
+(function kavinKarthick(){
+    let karthick=[1,2,3,4,5,6];
+    for(let i=0;i<karthick.length;i++){
+        if(karthick[i]%2!==0){
+            console.log(karthick[i]);
+        }
+    }
+})()
