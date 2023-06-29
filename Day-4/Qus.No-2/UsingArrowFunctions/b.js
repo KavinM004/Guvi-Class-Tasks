36ba087c8efd47495bdb9ffc91eb2bb2c20ca6d0
karthick <karthickkavin004@gmail.com>
2023-06-29 19:35:20 +0530

    hjh

--- Day-4/Qus.No-2/UsingArrowFunctions/b.js
+++ Day-4/Qus.No-2/UsingArrowFunctions/b.js
@@ -1,3 +1,10 @@
 let kavin=()=>{
-    
+    let kavin="kavin is good boy";
+let kar=kavin.split(" ");
+for(let i=0;i<kar.length;i++){
+    kar[i]=kar[i].charAt(0).toUpperCase()+kar[i].slice(1);
 }
+let nivak=kar.join(" ");
+console.log(nivak);
+}
+kavin();
