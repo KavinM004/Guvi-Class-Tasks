ee249b38e236edd2d605df96b93a46d550f6c0bb
karthick <karthickkavin004@gmail.com>
2023-06-29 19:33:04 +0530

    rfhy

---
+++ Day-4/Qus.No-2/UsingArrowFunctions/a.js
@@ -0,0 +1,9 @@
+let kavin=()=>{
+    let karthick=[1,2,3,4,5,6,7];
+    for(let i=0;i<karthick.length;i++){
+        if(karthick[i]%2!==0){
+            console.log(karthick[i]);
+        }
+    }
+}
+kavin();
