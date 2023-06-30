b6f94bee2419e5cac5ac15ae75708e9cea3c07d2
karthick <karthickkavin004@gmail.com>
2023-06-30 14:45:55 +0530

    Day-5

---
+++ Day-5/forIn.js
@@ -0,0 +1,8 @@
+(function kavin(){
+ let k='{"MyName":"Kavin M","Age":21,"Blood":"B+ve"}';
+ let a=JSON.parse(k);
+ for(let key in a){
+    let kar=a[key];
+    console.log(key+" "+":"+" "+kar);
+ }
+})()
---
+++ Day-5/forLoop.js
@@ -0,0 +1,3 @@
+let kavin='{"Myname":"Kavin M","Age":21}';
+let kar=JSON.parse(kavin);
+console.log(kar.Myname);thaga
