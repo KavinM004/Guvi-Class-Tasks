a9e359d1a744ee93acb766bc75bf47bf9cdac742
karthick <karthickkavin004@gmail.com>
2023-07-05 21:06:46 +0530

    Day-6

--- Day-6/Qus.No-3.js
+++ Day-6/Qus.No-3.js
@@ -1,6 +1,9 @@
-class Person{
-    Name : "Kavin M",
-    Age : 21,
-    Blood : "B+ve",
-    Address : "Salem";
-}
+let Person = {
+    MyName:'Kavin M',
+    Age:21,
+    Blood:'A',
+    Department: 'Computer Science and Engineering',
+    College:'KSR College of Engineering'
+  };
+  console.log(Person.MyName,Person.Age,Person.Blood,Person.Department,Person.College); 
+  
