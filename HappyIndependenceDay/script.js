c9f43595754d4330534a8bc543e27c9d32247d6f
karthick <karthickkavin004@gmail.com>
2023-08-06 19:43:58 +0530

    HappyIndependenceDay

--- HappyIndependenceDay/script.js
+++ HappyIndependenceDay/script.js
@@ -1,23 +1,58 @@
-let countdown = (number, callback) => {
-    let displayElement = document.getElementById("countdown");
-    let updateDisplay = () => {
-      displayElement.innerHTML = number;
-    };
-    let nextCountdown = () => {
-      if (number > 0) {
-        updateDisplay();
-        number--;
-        setTimeout(nextCountdown, 1000);
-      } else {
-        updateDisplay();
-        setTimeout(callback, 1000);
+// JavaScript code
+function display() {
+  // Function to display content in HTML
+  function updateOutput(content) {
+      let outputElement = document.getElementById('output');
+      if (outputElement) {
+          outputElement.innerHTML += content + '<br>';
       }
-    };
-    nextCountdown();
-  };
-  let happyIndependenceDay = () => {
-    let displayElement = document.getElementById("countdown");
-    displayElement.innerHTML = "Happy Independence Day";
-  };
-  countdown(10, happyIndependenceDay);
+  }
+
+  setTimeout(() => {
+      updateOutput("10");
+
+      setTimeout(() => {
+          updateOutput("9");
+
+          setTimeout(() => {
+              updateOutput("8");
+
+              setTimeout(() => {
+                  updateOutput("7");
+
+                  setTimeout(() => {
+                      updateOutput("6");
+
+                      setTimeout(() => {
+                          updateOutput("5");
+
+                          setTimeout(() => {
+                              updateOutput("4");
+
+                              setTimeout(() => {
+                                  updateOutput("3");
+
+                                  setTimeout(() => {
+                                      updateOutput("2");
+
+                                      setTimeout(() => {
+                                          updateOutput("1");
+
+                                          setTimeout(() => {
+                                              updateOutput("Happy Independence Day");
+                                          }, 1000);
+                                      }, 1000);
+                                  }, 1000);
+                              }, 1000);
+                          }, 1000);
+                      }, 1000);
+                  }, 1000);
+              }, 1000);
+          }, 1000);
+      }, 1000);
+  }, 0);
+}
+
+display();
+
   
