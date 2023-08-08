a9cfebf61db24f09cd67ae6b9aa86e019c7bfb21
karthick <karthickkavin004@gmail.com>
2023-08-08 17:40:46 +0530

    Day-17

--- Day-17/script.js
+++ Day-17/script.js
@@ -23,10 +23,23 @@ const getCountry = (country) =>{
       <h4 style="text-align:center;font-style:">Regoin : ${country.region}</h4>
       <h4 style="text-align:center;font-style:">Country Code : ${country.cca2}</h4>
       <button class="btn">Weather</button>
-      // btn.addEventListener("click",function(){
-      //     alert(api.openweathermap.org/data/2.5/forecast/daily?q=London&appid=893ce93d19df342cf40bbf0822359f8d);
-      // })
-      // </div>
+       </div>
   `
 }
 loadCountryAPI()
+
+let apiurl='https://api.openweathermap.org/data/2.5/weather?q=peru&appid=893ce93d19df342cf40bbf0822359f8d';
+fetch(apiurl).then((response)=>{
+    if(response.status===200){
+        return response.json();
+    }
+})
+.then((val)=>{
+    let kavin=document.getElementById("btn");
+    kavin.onclick=function(){
+        kavin.innerHTML=val.main.temp;
+    }
+})
+.catch((err)=>{
+    console.log("Error",err);
+})
